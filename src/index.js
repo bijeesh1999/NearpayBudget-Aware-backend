52c3737f232937acb03dfdc632ced37f2dc82e73
Bijeesh <bijeeshb.stackup@gmail.com>
2025-11-22 18:31:11 +0530

    cors fix

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import dotenv from "dotenv";
 import { connectMongoo } from "./config/db.config.js";
 import globalApiRoute from "./app.js";
 import cookieParser from "cookie-parser";
-import { ipFilter } from "./config/ip.js";
 
 dotenv.config();
 const app = express();
 
-app.set('trust proxy', 1); // Use 1 if you know Render uses one proxy level
+app.set("trust proxy", 1); // Use 1 if you know Render uses one proxy level
 app.use(express.json());
 app.use(cookieParser());
+const FRONTEND_ORIGIN = process.env.FRONTEND_URL || "http://localhost:3000";
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL, // Your frontend URL
-    credentials: true, // Allow credentials
+    origin: FRONTEND_ORIGIN,
+    credentials: true,
   })
 );
 connectMongoo();
@@ -23,5 +23,5 @@ connectMongoo();
 app.use("/api", globalApiRoute);
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(`http://localhost/${process.env.PORT || 3000}`);
+  console.log(`http://localhost:${process.env.PORT || 3000}`);
 });
