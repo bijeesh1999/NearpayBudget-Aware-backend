a7396ff797708308702c8ed503f20ba6076876db
Bijeesh <bijeeshb.stackup@gmail.com>
2025-11-22 18:55:17 +0530

    cors fix

--- src/app.js
+++ src/app.js
@@ -1,19 +1,15 @@
 import { Router } from "express";
-import userRouter from "./services/users/routes/route.js"
-import categoryRouter from "./services/categories/routes/route.js"
-import budgetRouter from "./services/budgets/routes/route.js"
-import expenseRouter from "./services/expenses/routes/route.js"
-import { authMiddleware } from "./middleware/auth.js"
+import userRouter from "./services/users/routes/route.js";
+import categoryRouter from "./services/categories/routes/route.js";
+import budgetRouter from "./services/budgets/routes/route.js";
+import expenseRouter from "./services/expenses/routes/route.js";
+import { authMiddleware } from "./middleware/auth.js";
 
+const router = Router();
 
+router.use("/user", userRouter);
+router.use("/category", authMiddleware, categoryRouter);
+router.use("/budget", authMiddleware, budgetRouter);
+router.use("/expense", authMiddleware, expenseRouter);
 
-const router = Router()
-
-
-router.use("/user", userRouter)
-router.use("/category", categoryRouter)
-router.use("/budget", authMiddleware, budgetRouter)
-router.use("/expense", authMiddleware, expenseRouter)
-
-
-export default router
+export default router;
